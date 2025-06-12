0efda33a6982f85ff4b26a786a786dab6d98cd96
Saad <saaw92@zedat.fu-berlin.de>
2025-06-12 13:41:46 +0200

    Added dependencies

--- server/app/lr_model.py
+++ server/app/lr_model.py
@@ -1,3 +1,6 @@
+import os
 import joblib
 
-lr_model = joblib.load('lr_model.joblib')
+lr_model_path = os.path.join(os.path.dirname(__file__), 'lr_model.joblib')
+
+lr_model = joblib.load(lr_model_path)
