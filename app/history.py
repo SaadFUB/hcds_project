5025a42b0c005e07eec8f8e9786428d2c9e1b7f9
Yasemin Mutlugil <yaseminmutlugil@hotmail.com>
2025-06-28 17:12:13 +0200

    Updated resources, methodology and team pages

--- app/history.py
+++ app/history.py
@@ -2,3 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title("History")
+
+st.markdown("History of predictions will appear here.")
--- app/methodology.py
+++ app/methodology.py
@@ -2,38 +2,43 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# st.title("How does our tool work?")
+st.markdown("<h2 style='text-align: center;'>How does our tool work?</h2>", unsafe_allow_html=True)
+st.markdown("---")
 
+# st.markdown("#### Models", unsafe_allow_html=True)
+
+st.title("Models used")
 with st.container(border=True):
 
     left, middle, right = st.columns([1, 2, 1])
 
-    with middle: 
-        st.title("How does our tool work?")
+    # with middle: 
+    #     st.title("How does our tool work?")
     
-    st.write("Models")
+    # st.markdown("#### Models", unsafe_allow_html=True)
 
     left, middle, right = st.columns([1, 1, 1])
 
     with left:
-        st.write("Logistic Regression")
-        st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse facilisis eros ac neque pulvinar, in convallis leo ullamcorper. Vestibulum aliquam ut eros ut placerat. Integer ullamcorper eros ut odio venenatis sollicitudin. Cras pulvinar augue eget tortor mollis dapibus. Aenean a nisl diam. Fusce imperdiet risus augue. In eu dui vel justo dictum congue vitae ac ante. Aenean lacinia felis lectus, in luctus leo dignissim vel. Vestibulum sollicitudin eleifend dui, volutpat luctus augue pellentesque eget. In semper enim massa, nec dictum risus ornare ut. Quisque ut mattis est. Interdum et malesuada fames ac ante ipsum primis in faucibus. ")
+        st.markdown("<div style='text-align: center; font-weight: bold;'>Logistic Regression</div><br>", unsafe_allow_html=True)
+        st.markdown("<div style='text-align: center;'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse facilisis eros ac neque pulvinar, in convallis leo ullamcorper. Vestibulum aliquam ut eros ut placerat. Integer ullamcorper eros ut odio venenatis sollicitudin. Cras pulvinar augue eget tortor mollis dapibus. Aenean a nisl diam. Fusce imperdiet risus augue. In eu dui vel justo dictum congue vitae ac ante. Aenean lacinia felis lectus, in luctus leo dignissim vel. Vestibulum sollicitudin eleifend dui, volutpat luctus augue pellentesque eget. In semper enim massa, nec dictum risus ornare ut. Quisque ut mattis est. Interdum et malesuada fames ac ante ipsum primis in faucibus. </div>", unsafe_allow_html=True)
     with middle: 
-        st.write("Random Forest")
-        st.write("Cras id augue nisl. Pellentesque ut nisi nec ipsum accumsan aliquam congue sed neque. Nullam purus lorem, viverra vel viverra at, iaculis eu lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse potenti. Donec nec odio volutpat, sollicitudin orci vel, interdum tellus. Donec felis quam, tempor interdum posuere eu, varius in nulla. Fusce elementum mauris vel nulla rhoncus porttitor. Cras commodo sagittis augue sed sagittis. Curabitur efficitur efficitur risus in pharetra. Proin volutpat convallis lorem in eleifend. Etiam ut auctor neque. Aenean tincidunt, enim in eleifend malesuada, diam leo hendrerit diam, vitae rhoncus leo risus eget massa. Suspendisse at ante pellentesque, consectetur arcu at, semper nibh. Etiam a metus massa. ")
+        st.markdown("<div style='text-align: center; font-weight: bold;'>Random Forest</div><br>", unsafe_allow_html=True)
+        st.markdown("<div style='text-align: center;'>Cras id augue nisl. Pellentesque ut nisi nec ipsum accumsan aliquam congue sed neque. Nullam purus lorem, viverra vel viverra at, iaculis eu lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse potenti. Donec nec odio volutpat, sollicitudin orci vel, interdum tellus. Donec felis quam, tempor interdum posuere eu, varius in nulla. Fusce elementum mauris vel nulla rhoncus porttitor. Cras commodo sagittis augue sed sagittis. Curabitur efficitur efficitur risus in pharetra. Proin volutpat convallis lorem in eleifend. Etiam ut auctor neque. Aenean tincidunt, enim in eleifend malesuada, diam leo hendrerit diam, vitae rhoncus leo risus eget massa. Suspendisse at ante pellentesque, consectetur arcu at, semper nibh. Etiam a metus massa. </div>", unsafe_allow_html=True)
     with right:
-        st.write("Neural Network")
-        st.write("Nullam convallis urna non leo sollicitudin, non suscipit massa molestie. Vivamus mollis semper viverra. Integer condimentum libero et neque imperdiet, non efficitur mi posuere. Morbi vel eros fringilla, aliquam enim ut, mollis elit. Ut pharetra hendrerit ornare. Aenean et ipsum id augue ultricies consectetur at et nisl. Quisque mauris neque, blandit id mauris eget, luctus lobortis urna. Aenean eu nisi vitae leo commodo laoreet. ")
+        st.markdown("<div style='text-align: center; font-weight: bold;'>Neural Network</div><br>", unsafe_allow_html=True)
+        st.markdown("<div style='text-align: center;'>Nullam convallis urna non leo sollicitudin, non suscipit massa molestie. Vivamus mollis semper viverra. Integer condimentum libero et neque imperdiet, non efficitur mi posuere. Morbi vel eros fringilla, aliquam enim ut, mollis elit. Ut pharetra hendrerit ornare. Aenean et ipsum id augue ultricies consectetur at et nisl. Quisque mauris neque, blandit id mauris eget, luctus lobortis urna. Aenean eu nisi vitae leo commodo laoreet. </div>", unsafe_allow_html=True)
 
     left, middle, right = st.columns([1, 1, 1])
 
-        
+# st.markdown("#### Training dataset and metadata", unsafe_allow_html=True)
 
-    st.write("Training dataset")
-    st.write("Nullam convallis urna non leo sollicitudin, non suscipit massa molestie. Vivamus mollis semper viverra. Integer condimentum libero et neque imperdiet, non efficitur mi posuere. Morbi vel eros fringilla, aliquam enim ut, mollis elit. Ut pharetra hendrerit ornare. Aenean et ipsum id augue ultricies consectetur at et nisl. Quisque mauris neque, blandit id mauris eget, luctus lobortis urna. Aenean eu nisi vitae leo commodo laoreet. ")
+# Read and display metadata.md
+with open("../metadata.md", "r") as f:
+    metadata_content = f.read()
+st.markdown(metadata_content, unsafe_allow_html=True)
 
-    st.write("Description and metadata")
-    st.write("Nullam convallis urna non leo sollicitudin, non suscipit massa molestie. Vivamus mollis semper viverra. Integer condimentum libero et neque imperdiet, non efficitur mi posuere. Morbi vel eros fringilla, aliquam enim ut, mollis elit. Ut pharetra hendrerit ornare. Aenean et ipsum id augue ultricies consectetur at et nisl. Quisque mauris neque, blandit id mauris eget, luctus lobortis urna. Aenean eu nisi vitae leo commodo laoreet. ")
 
 
-    
 
