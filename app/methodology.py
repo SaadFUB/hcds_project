903a1e3132278d20d17cfb9dd5ae37f370113f0d
Nino <126103139+ninosabella20@users.noreply.github.com>
2025-06-30 19:42:03 +0200

    Update methodology.py

--- app/methodology.py
+++ app/methodology.py
@@ -2,38 +2,61 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# st.title("How does our tool work?")
+st.markdown("<h2 style='text-align: center;'>How does our tool work?</h2>", unsafe_allow_html=True)
+st.markdown("---")
 
+# st.markdown("#### Models", unsafe_allow_html=True)
+
+st.title("Models used")
 with st.container(border=True):
 
     left, middle, right = st.columns([1, 2, 1])
 
-    with middle: 
-        st.title("How does our tool work?")
+    # with middle: 
+    #     st.title("How does our tool work?")
     
-    st.write("Models")
+    # st.markdown("#### Models", unsafe_allow_html=True)
 
     left, middle, right = st.columns([1, 1, 1])
 
     with left:
-        st.write("Logistic Regression")
-        st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse facilisis eros ac neque pulvinar, in convallis leo ullamcorper. Vestibulum aliquam ut eros ut placerat. Integer ullamcorper eros ut odio venenatis sollicitudin. Cras pulvinar augue eget tortor mollis dapibus. Aenean a nisl diam. Fusce imperdiet risus augue. In eu dui vel justo dictum congue vitae ac ante. Aenean lacinia felis lectus, in luctus leo dignissim vel. Vestibulum sollicitudin eleifend dui, volutpat luctus augue pellentesque eget. In semper enim massa, nec dictum risus ornare ut. Quisque ut mattis est. Interdum et malesuada fames ac ante ipsum primis in faucibus. ")
+        st.markdown("<div style='text-align: center; font-weight: bold;'>Logistic Regression</div><br>", unsafe_allow_html=True)
+        st.markdown("""<div style='text-align: center;'> A statistical model that predicts the probability of a binary outcome (like "at risk" or "not at risk") based on input features. It’s simple, fast, and works well when the relationship between features and outcome is mostly linear. </div>""", unsafe_allow_html=True)
     with middle: 
-        st.write("Random Forest")
-        st.write("Cras id augue nisl. Pellentesque ut nisi nec ipsum accumsan aliquam congue sed neque. Nullam purus lorem, viverra vel viverra at, iaculis eu lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse potenti. Donec nec odio volutpat, sollicitudin orci vel, interdum tellus. Donec felis quam, tempor interdum posuere eu, varius in nulla. Fusce elementum mauris vel nulla rhoncus porttitor. Cras commodo sagittis augue sed sagittis. Curabitur efficitur efficitur risus in pharetra. Proin volutpat convallis lorem in eleifend. Etiam ut auctor neque. Aenean tincidunt, enim in eleifend malesuada, diam leo hendrerit diam, vitae rhoncus leo risus eget massa. Suspendisse at ante pellentesque, consectetur arcu at, semper nibh. Etiam a metus massa. ")
+        st.markdown("<div style='text-align: center; font-weight: bold;'>Random Forest</div><br>", unsafe_allow_html=True)
+        st.markdown("""<div style='text-align: center;'> An ensemble learning method that builds many decision trees and combines their results to make a more accurate and robust prediction. It’s good at handling complex data and reducing overfitting. </div>""", unsafe_allow_html=True)
     with right:
-        st.write("Neural Network")
-        st.write("Nullam convallis urna non leo sollicitudin, non suscipit massa molestie. Vivamus mollis semper viverra. Integer condimentum libero et neque imperdiet, non efficitur mi posuere. Morbi vel eros fringilla, aliquam enim ut, mollis elit. Ut pharetra hendrerit ornare. Aenean et ipsum id augue ultricies consectetur at et nisl. Quisque mauris neque, blandit id mauris eget, luctus lobortis urna. Aenean eu nisi vitae leo commodo laoreet. ")
+        st.markdown("<div style='text-align: center; font-weight: bold;'>Neural Network</div><br>", unsafe_allow_html=True)
+        st.markdown("""<div style='text-align: center;'>A machine learning model inspired by the human brain, made up of layers of interconnected nodes (“neurons”). It can learn complex patterns in data, making it powerful for capturing subtle relationships between features. </div>""", unsafe_allow_html=True)
 
     left, middle, right = st.columns([1, 1, 1])
 
-        
+# st.markdown("#### Training dataset and metadata", unsafe_allow_html=True)
 
-    st.write("Training dataset")
-    st.write("Nullam convallis urna non leo sollicitudin, non suscipit massa molestie. Vivamus mollis semper viverra. Integer condimentum libero et neque imperdiet, non efficitur mi posuere. Morbi vel eros fringilla, aliquam enim ut, mollis elit. Ut pharetra hendrerit ornare. Aenean et ipsum id augue ultricies consectetur at et nisl. Quisque mauris neque, blandit id mauris eget, luctus lobortis urna. Aenean eu nisi vitae leo commodo laoreet. ")
+# Reading and displaying metadata from Git repo
+with open("../metadata.md", "r") as f:
+    metadata_content = f.read()
+st.markdown(metadata_content, unsafe_allow_html=True)
 
-    st.write("Description and metadata")
-    st.write("Nullam convallis urna non leo sollicitudin, non suscipit massa molestie. Vivamus mollis semper viverra. Integer condimentum libero et neque imperdiet, non efficitur mi posuere. Morbi vel eros fringilla, aliquam enim ut, mollis elit. Ut pharetra hendrerit ornare. Aenean et ipsum id augue ultricies consectetur at et nisl. Quisque mauris neque, blandit id mauris eget, luctus lobortis urna. Aenean eu nisi vitae leo commodo laoreet. ")
+st.markdown("### Other important concepts")
+
+with st.container():
+    left, right = st.columns(2)
+
+    with left:
+        st.markdown("<div style='font-weight: bold;'>Confusion Matrix</div>", unsafe_allow_html=True)
+        st.markdown("A table that shows how well a model did at telling apart two different occurrences (binary: 0 or 1). It counts the number of correct and wrong predictions. In our model cases, it refers to how many times the Alzheimer’s diagnosis was correct and how many times it was not.", unsafe_allow_html=True)
+
+        st.markdown("<div style='font-weight: bold;'>Model Accuracy</div>", unsafe_allow_html=True)
+        st.markdown("The percentage of times a model was right when predicting if someone has Alzheimer's or not. The higher the accuracy, the better the model is at making correct predictions. The accuracy formula is correct decisions divided by all decisions.", unsafe_allow_html=True)
+
+    with right:
+        st.markdown("<div style='font-weight: bold;'>Fairness</div>", unsafe_allow_html=True)
+        st.markdown("A model is fair if it treats different groups of people equally. Our models should work just as well for patients of different education levels or genders without bias. Fairness is a key pillar of ethical decision making.", unsafe_allow_html=True)
+
+        st.markdown("<div style='font-weight: bold;'>Feature Importance</div>", unsafe_allow_html=True)
+        st.markdown("Shows which factors in the data matter most for predicting Alzheimer's. For example, it might show that memory test scores or brain scan results have a big impact on the prediction.", unsafe_allow_html=True)
 
 
-    
 
